5bec10ad708a83cebec809ee980849c31cefc7db
bwvick <bwv0004@tigermail.auburn.edu>
2017-10-06 16:10:44 -0500

    Added BinaryToHex and IntToBinary functions

--- FireAnts/FireAntsNetBeans.java
+++ FireAnts/FireAntsNetBeans.java
@@ -36,17 +36,17 @@ public class FireAntsNetBeans {
               
               //j type
               if (line.substring(0,1).equals("j")) {
-                  machineInstructions.add(processJType(line));                                 
+                  machineInstructions.add(parseJType(line));                                 
               }
               
               //r type
               else if (Arrays.asList(rInstructions).contains(line.substring(0,3))) {
-                  machineInstructions.add(processRType(line));
+                  machineInstructions.add(parseRType(line));
               }
               
               //i type
               else if (Arrays.asList(iInstructions).contains(line.substring(0,3))) {
-                  machineInstructions.add(processIType(line));
+                  machineInstructions.add(parseIType(line));
               }
                                  
               line=br.readLine();
@@ -66,17 +66,35 @@ public class FireAntsNetBeans {
       
     }
     
-    private static String processJType(String instruction) {  
+    private static String parseJType(String instruction) {  
+        //j instruction binary format:        
+        //address(26), opcode(6)
+        //possible instructions
+        //j address
         
         return "J " + instruction;
     }
     
-    private static String processRType(String instruction) {
-        
+    private static String parseRType(String instruction) {
+        //R instruction binary format:        
+        //function(6), shamt(5), rd(5), rs(5), rt(5), opcode(6)
+        //possible instructions (spaces between arguments optional)
+        //function and shamt will always be all 0
+        //add $rd, $rs, $rt - opcode=10
+        //sub $rd, $rs, $rt - opcode=20
+        //slt $rd, $rs, $rt - opcode=30
+        //and $rd, $rs, $rt - opcode=40
+        //nor $rd, $rs, $rt - opcode=50
         return "R " + instruction;
     }
     
-    private static String processIType(String instruction) {
+    private static String parseIType(String instruction) {
+        //i instruction binary format:        
+        //offset(16), rs(5), rt(5), opcode(6)
+        //possible instructions (spaces between arguments optional)
+        //lwd $rt, offset($rs) - opcode=35
+        //swd $rt, offset($rs) - opcode=43
+        //beq $rt, $rs, offset - opcode=2
         
         return "I " + instruction;
     }
@@ -105,6 +123,26 @@ public class FireAntsNetBeans {
         } catch (IOException e) {
       e.printStackTrace();
       } 
+    }
+    
+    private static String IntToBinary(int num, int bits) {
+        //convert an interger to binary and pad to a specific number of bits
+        String binString = Integer.toBinaryString(num);
+        while (binString.length() < bits) {    //pad with 16 0's
+            binString = "0" + binString;
+        }
+        return binString;
+    }
+    
+    private static String BinaryToHex(String bin) {
+        int decimal = Integer.parseInt(bin,2);
+        String hexString = Integer.toString(decimal,16).toUpperCase();
+        while (hexString.length() < 8) {
+            hexString = "0" + hexString;
+        }
+        hexString = "0x" + hexString;
+        
+        return hexString;
     }
 }
 
